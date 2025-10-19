18f8512313a98e320f1dc66780b30264bae2f01a
fernanda <fer.r.benetti@gmail.com>
2025-10-19 19:43:18 -0300

    fazendo contador de links

--- biblioteca_frontend/src/App.jsx
+++ biblioteca_frontend/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-          <h1>Seja bem vindo(a) {nome}</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deleniti minima pariatur sunt est fugit sint laudantium cumque adipisci consectetur facilis et quam doloremque iusto corporis natus, doloribus beatae consequuntur?</p>
+      <h1>Seja bem vindo(a) {nome}</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deleniti minima pariatur sunt est fugit sint laudantium cumque adipisci consectetur facilis et quam doloremque iusto corporis natus, doloribus beatae consequuntur?</p>
     </>
   )
 }
